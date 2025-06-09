e094899886c9453f0b7c5346fdbe597db1b96431
thaohuynh <huynhngocthao989@gmail.com>
2025-06-09 22:43:09 +0700

    ATP-9 update POM and Config

--- src/main/java/dev/orangehrm/utils/ConfigLoader.java
+++ src/main/java/dev/orangehrm/utils/ConfigLoader.java
@@ -1,4 +1,31 @@
 package dev.orangehrm.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigLoader {
+    private static ConfigLoader configLoader;
+    private Properties properties;
+
+    private ConfigLoader() {
+        properties = new Properties();
+        try {
+            FileInputStream input = new FileInputStream("D:\\HKV\\orangehrm-automation-tests\\src\\test\\resources\\dev_config.properties");
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ConfigLoader getInstance() {
+        if (configLoader == null) {
+            configLoader = new ConfigLoader();
+        }
+        return configLoader;
+    }
+
+    public String getProperty (String propertyName){
+        return properties.getProperty(propertyName);
+    }
 }
